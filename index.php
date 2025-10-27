b9a7fc85354f40f3e7bcdb770b9ef8600ca98bf4
carissaura <carissaaura0505@gmail.com>
2025-10-28 04:56:19 +0700

    Upload File Proyek DevOps
    File Index, K8s, dan Docker

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Hello from PHP App!</h1>";
+echo "<p>Nama: Carissa Aura Bethari</p>";
+echo "<p>NIM: 2210512046</p>";
+echo "<p>Kelas A Cloud Computing - S1 Sistem Informasi</p>";
+echo "<p>Deployed via DevOps CI/CD pipeline</p>";
+?>
